051eb47d90e3682565d32ca66ec301c289541568
Handi10 <Handiprasetia10@gmail.com>
2021-12-05 17:58:30 +0700

    initial commit

---
+++ application/views/Autentifikasi/login.php
@@ -0,0 +1,62 @@
+<div class="container">
+
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
+
+        <div class="col-xl-5 col-lg-12 col-md-6">
+
+            <div class="card o-hidden border-0 shadow-lg my-5">
+                <div class="card-body p-0">
+                    <!-- Nested Row within Card Body -->
+                    <div class="row">
+                        <!-- GANTI TAMPILAN GAMBARNYA -->
+                        <!-- <div class="col-lg-6 d-none d-lg-block bg-login-image"></div> -->
+                        <div class="col-lg-12">
+                            <div class="p-5">
+                                <div class="text-center">
+                                    <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
+                                </div>
+
+                                <?= $this->session->flashdata('message'); ?>
+
+                                <form class="user" method="post" action="<?= base_url('autentifikasi'); ?>">
+                                    <div class="form-group">
+                                        <input type="text" class="form-control form-control-user" id="email" name="email" placeholder="Enter Email Address..." value="<?= set_value('email') ?>">
+                                        <?= form_error('email', '<small class="text-danger pl-3">', '</small>'); ?>
+                                    </div>
+                                    <div class="form-group">
+                                        <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
+                                        <?= form_error('password', '<small class="text-danger pl-3">', '</small>'); ?>
+                                    </div>
+                                    <button type="submit" class="btn btn-primary btn-user btn-block">
+                                        Login
+                                    </button>
+                                    <hr>
+                                </form>
+                                <div class="text-center">
+                                    <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                </div>
+                                <div class="text-center">
+                                    <a class="small" href="<?= base_url('autentifikasi/registration'); ?>">Create an Account!</a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+</div>
+<br><br><br><br><br><br><br><br>
+
+<div class="text-center">
+<a href="<?= base_url('autentifikasi/home'); ?>" style="color:white">Home | </a>
+<a href="<?= base_url('autentifikasi/about'); ?>" style="color:white">About us |</a>
+<a href="<?= base_url('autentifikasi/contact'); ?>" style="color:white">Contact us</a>
+    <br>
+
+
+                                </div>
